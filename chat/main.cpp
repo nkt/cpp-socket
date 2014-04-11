16280a42f8cbf2f472c982c8818de13a6954b484
Gusakov Nikita <dev@nkt.me>
2014-04-11 23:07:13 +0400

    Initial Commit

---
+++ chat/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  chat
+//
+//  Created by Gusakov Nikita on 11/04/14.
+//  Copyright (c) 2014 nkt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
